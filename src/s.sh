405be075b3ce6257d16629c6b210a9838d2f4b53
Rishab-kulkarni <rishabkulkarni1703@gmail.com>
2021-11-11 08:05:28 +0530

    added LLVM kaleidoscope

--- src/s.sh
+++ src/s.sh
@@ -2,12 +2,9 @@
 
 
 
-g++ -w -std=c++14 -g -o parser *.cpp `llvm-config --cxxflags --ldflags --libs --libfiles --system-libs` 
+#g++ -w -std=c++14 -g -o parser *.cpp `llvm-config --cxxflags --ldflags --libs --libfiles --system-libs` 
+
+g++ -w -std=c++14 -g -o a kaleidoscope.cpp `llvm-config --cxxflags --ldflags --libs --libfiles --system-libs` 
 
 
-#g++ -w -std=c++14 -g -o codegen.cpp `llvm-config --cxxflags --ldflags --libs --libfiles --system-libs`
-
-#g++ -w -std=c++14 -g -o parser parser.cpp tokens.cpp main.cpp codegen.cpp `llvm-config --cxxflags --ldflags --libs --libfiles --system-libs`
-
-#g++ -w -std=c++14 -g parser.cpp tokens.cpp main.cpp -o a `llvm-config --cxxflags --ldflags --libs --libfiles --system-libs` 
 
